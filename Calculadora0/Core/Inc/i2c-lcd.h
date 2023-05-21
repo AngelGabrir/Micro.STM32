6e1769321183829ba4a61270da6b8de90a589d3b
Angel <angelgabrir23@gmail.com>
2023-05-21 00:24:46 -0400

    1.2

--- Calculadora0/Core/Inc/i2c-lcd.h
+++ Calculadora0/Core/Inc/i2c-lcd.h
@@ -1,4 +1,4 @@
-#include "stm32f1xx_hal.h"
+#include "stm32f3xx_hal.h"
 
 void lcd_init (void);   // initialize lcd
 
--- Calculadora0/Core/Src/main.c
+++ Calculadora0/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "i2c-lcd.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,7 +33,29 @@
 /* USER CODE BEGIN PD */
 
 /* USER CODE END PD */
+#define R1_PORT GPIOA
+#define R1_PIN GPIO_PIN_8
 
+#define R2_PORT GPIOB
+#define R2_PIN GPIO_PIN_10
+
+#define R3_PORT GPIOB
+#define R3_PIN GPIO_PIN_4
+
+#define R4_PORT GPIOB
+#define R4_PIN GPIO_PIN_5
+
+#define C1_PORT GPIOB
+#define C1_PIN GPIO_PIN_3
+
+#define C2_PORT GPIOA
+#define C2_PIN GPIO_PIN_10
+
+#define C3_PORT GPIOA
+#define C3_PIN GPIO_PIN_2
+
+#define C4_PORT GPIOA
+#define C4_PIN GPIO_PIN_3
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 
